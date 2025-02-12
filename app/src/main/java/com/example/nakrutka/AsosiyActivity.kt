5ba284ec543e3f7019d7733ee89659d71089cbd4
MAAB <orzubekjumaniyozov2010@gmail.com>
2025-02-12 17:53:21 +0500

    9999

--- app/src/main/java/com/example/nakrutka/AsosiyActivity.kt
+++ app/src/main/java/com/example/nakrutka/AsosiyActivity.kt
@@ -1,16 +1,40 @@
 package com.example.nakrutka
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.nakrutka.databinding.ActivityAsosiyBinding
 
 class AsosiyActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAsosiyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_asosiy)
-
-    }
+            binding = ActivityAsosiyBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView2,
+                AsosiyFragment()).commit()
+binding.navView.setOnItemSelectedListener {
+              when(it.itemId) {
+                  R.id.homenav -> {
+                      supportFragmentManager.beginTransaction()
+                          .replace(R.id.fragmentContainerView2,
+                              AsosiyFragment()).commit()
+                      return@setOnItemSelectedListener true
+                  }
+                  R.id.profilnav -> {
+                      supportFragmentManager.beginTransaction()
+                          .replace(R.id.fragmentContainerView2,
+                             ProfilFragment()).commit()
+                      return@setOnItemSelectedListener true
+                  }
+                  else -> false
+              }
+               return@setOnItemSelectedListener true
 }
+            }
+    }

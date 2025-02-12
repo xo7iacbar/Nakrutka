8ee8912ee4482cf4236b7706fea030f30dfc289f
MAAB <orzubekjumaniyozov2010@gmail.com>
2025-02-12 17:53:21 +0500

    12

--- app/src/main/java/com/example/nakrutka/KirishFragment.kt
+++ app/src/main/java/com/example/nakrutka/KirishFragment.kt
@@ -1,10 +1,13 @@
 package com.example.nakrutka
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.nakrutka.databinding.FragmentKirishBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +20,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class KirishFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+   lateinit var binding: FragmentKirishBinding
     private var param1: String? = null
     private var param2: String? = null
 
@@ -33,8 +36,17 @@ class KirishFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kirish, container, false)
+        binding=FragmentKirishBinding.inflate(LayoutInflater.from(container?.context),container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.kirishbtn.setOnClickListener {
+            val intent = Intent(activity, AsosiyActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/nakrutka/LoginFragment.kt
+++ app/src/main/java/com/example/nakrutka/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.example.nakrutka
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.nakrutka.databinding.FragmentKirishBinding
+import com.example.nakrutka.databinding.FragmentLoginBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +20,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+  lateinit var binding: FragmentLoginBinding
     private var param1: String? = null
     private var param2: String? = null
 
@@ -29,14 +32,24 @@ class LoginFragment : Fragment() {
         }
     }
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+      binding=FragmentLoginBinding.inflate(LayoutInflater.from(container?.context),container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.royxatdanotishbtn.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,KirishFragment()).commit()
+        }
+        binding.otgantxt.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,KirishFragment()).commit()
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
